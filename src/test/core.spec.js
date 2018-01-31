0d06fd982bdc9836f738e1f0b4c0e3ce00c443a8
jorden <www.freewm@163.com>
2018-01-31 18:02:43 +0800

    test is success

---
+++ src/test/core.spec.js
@@ -0,0 +1,16 @@
+import core from './../core';
+
+describe('core测试套件',function(){
+    it('core.extends',function(){
+        var targe={
+            a:1
+        },
+        source={
+            a:2,
+            b:1
+        };
+
+        var result = core.extend(targe,source);
+        expect(source).toEqual(source);
+    })
+})
--- src/test/sum.spec.js
+++
@@ -1,8 +0,0 @@
-import sum from './../sum';
-
-describe('第一个测试套件',function(){
-    it('第一个测试用例:1+1 === 2',function(){
-        var result = sum(1,2)
-        expect(result).toBe(3);
-    })
-})
